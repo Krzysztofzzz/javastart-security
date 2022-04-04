b86876e0aea08e4b81584e520946d5e344d2d6ff
Krzysztof <krzubrycki@gmail.com>
2022-04-04 15:36:04 +0200

    overriding login site

---
+++ src/main/java/com/javastart/sec/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.javastart.sec;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests(requests -> requests.anyRequest().authenticated());
+        http.csrf().disable();
+/*        http.formLogin(login -> login
+                .loginPage("/logowanie")
+                .usernameParameter("user")
+                .passwordParameter("pass")
+                .permitAll());*/
+        http.formLogin(login -> login.loginPage("/login").permitAll());
+    }
+
+}
---
+++ src/main/java/com/javastart/sec/web/LoginController.java
@@ -0,0 +1,13 @@
+package com.javastart.sec.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    String loginForm() {
+        return "login-form";
+    }
+}
