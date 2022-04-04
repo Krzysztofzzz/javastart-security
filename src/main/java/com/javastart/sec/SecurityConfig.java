80e0c300098dfbe8028b7467e3ee00d90eea2ca1
Krzysztof <krzubrycki@gmail.com>
2022-04-04 16:03:54 +0200

    creating bean in config

--- src/main/java/com/javastart/sec/SecurityConfig.java
+++ src/main/java/com/javastart/sec/SecurityConfig.java
@@ -1,11 +1,14 @@
 package com.javastart.sec;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 
 @Configuration
@@ -17,13 +20,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.formLogin(login -> login.loginPage("/login").permitAll());
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        UserDetailsManager userDetailsService = auth.inMemoryAuthentication().getUserDetailsService();
+    @Bean
+    public UserDetailsService customUserDetailsService(){
         User.UserBuilder userBuilder = User.builder();
         UserDetails admin = userBuilder.username("superadmin").password("{noop}hard").roles("ADMIN").build();
         UserDetails user1 = userBuilder.username("john").password("{noop}asdf1234").roles("USER").build();
-        userDetailsService.createUser(admin);
-        userDetailsService.createUser(user1);
+        return new InMemoryUserDetailsManager(admin,user1);
     }
 }
