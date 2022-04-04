5f52e7e091b760942d6b3e5db80a640f5f190b7d
Krzysztof <krzubrycki@gmail.com>
2022-04-04 16:26:15 +0200

    security context #1

--- src/main/java/com/javastart/sec/web/HomeController.java
+++ src/main/java/com/javastart/sec/web/HomeController.java
@@ -1,6 +1,8 @@
 package com.javastart.sec.web;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.CurrentSecurityContext;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,8 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
     @GetMapping("/")
-    String home(Authentication authentication, Model model) {
-        model.addAttribute("username", authentication.getName());
+    String home(@CurrentSecurityContext SecurityContext securityContext, Model model) {
+        model.addAttribute("username", securityContext.getAuthentication().getName());
         return "index";
     }
 }
