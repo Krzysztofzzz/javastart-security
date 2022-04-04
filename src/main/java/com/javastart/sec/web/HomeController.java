c795643693b3e8a017afac538aaa265b2552835d
Krzysztof <krzubrycki@gmail.com>
2022-04-04 16:38:56 +0200

    presenting name on home through service with different languages

--- src/main/java/com/javastart/sec/web/HomeController.java
+++ src/main/java/com/javastart/sec/web/HomeController.java
@@ -1,16 +1,24 @@
 package com.javastart.sec.web;
 
+import com.javastart.sec.message.WelcomeMessageService;
 import org.springframework.security.core.annotation.CurrentSecurityContext;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
+    private WelcomeMessageService welcomeMessageService;
+
+    public HomeController(WelcomeMessageService welcomeMessageService) {
+        this.welcomeMessageService = welcomeMessageService;
+    }
+
     @GetMapping("/")
-    String home(@CurrentSecurityContext(expression = "authentication.name") String username, Model model) {
-        model.addAttribute("username", username);
+    String home(@RequestParam(defaultValue = "en") String lang, Model model) {
+        String welcomeMessage = welcomeMessageService.getWelcomeMessage(lang);
+        model.addAttribute("welcomeMessage", welcomeMessage);
         return "index";
     }
 }
