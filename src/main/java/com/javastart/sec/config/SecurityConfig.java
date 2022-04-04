7cece6645db8338113f6271ab7f5b971793cffd1
Krzysztof <krzubrycki@gmail.com>
2022-04-04 16:07:23 +0200

    defining users in component

---
+++ src/main/java/com/javastart/sec/config/SecurityConfig.java
@@ -0,0 +1,15 @@
+package com.javastart.sec.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests(requests -> requests.anyRequest().authenticated());
+        http.csrf().disable();
+        http.formLogin(login -> login.loginPage("/login").permitAll());
+    }
+}
