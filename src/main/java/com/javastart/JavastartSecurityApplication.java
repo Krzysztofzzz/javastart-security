b6af5bd1e4e6c4bf97f0d27bb62f2ade062bf8fc
Krzysztof <krzubrycki@gmail.com>
2022-04-04 14:58:12 +0200

    created project

---
+++ src/main/java/com/javastart/JavastartSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.javastart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavastartSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavastartSecurityApplication.class, args);
+    }
+
+}
